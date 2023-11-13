66798c9e186c41386c3317b1c5d753338894dfeb
aCarlos17 <149914132+aCarlos17@users.noreply.github.com>
2023-11-13 12:42:01 +0800

    add questions

---
+++ pages/start/start.js
@@ -0,0 +1,31 @@
+let p = 0
+let c = 0
+let a = 0
+let t = 0
+
+let programming = [
+    "I am confident in my mathematical skills.",
+    "I am intrigued by things such as technology or how games are made.",
+    "I am confident in my problem-solving abilities and my logical thinking.",
+    "I can easily understand and apply complex principles and concepts of anything at hand.",
+    "I can explain my ideas and my thought process in a way that anyone can understand.",
+    "The thought of creating products that would ease productivity interests me.",
+    "I am open to learn new things outside of my current scope.",
+    "I can easily implement and apply a broad concept, or a mechanic from an idea.",
+    "I am patient and detail-oriented when it comes to my tasks or projects."
+]
+
+let cookery = [
+    "I want to improve my experience in preparing food",
+    " "
+]
+
+let animation = [
+    "I am interested on how drawings come to life."
+]
+
+let tourism = [
+    "idk, tourism stuff ig"
+]
+
+console.log(p);
