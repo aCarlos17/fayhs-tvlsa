57c4b406cf4d17cc1f16f4386ec0027b00939c1f
aCarlos17 <149914132+aCarlos17@users.noreply.github.com>
2023-12-08 10:32:46 +0800

    Add tablet display support on result page

--- pages/start/result/result.js
+++ pages/start/result/result.js
@@ -17,6 +17,8 @@ let anme_img = '../../../images/animation.png'
 
 // img.src = prog_img <--- an example on how to change img using js
 
+img.src = prog_img
+
 //strand title
 let prog_title = "<h2>Programming</h2><h3>Information Communications Technology (ICT)</h3>"
 let cook_title = "<h2>Cookery</h2><h3>Home Economics (HE)</h3>"
@@ -25,8 +27,12 @@ let anme_title = "<h2>Animation</h2><h3>Information Communications Technology (I
 
 // strand.innerHTML = prog_title <--- an example of how to change text using js
 
+strand.innerHTML = prog_title
+
 //strand description
 let prog_desc = "This strand teaches an introduction to HTML5, CSS3, and JavaScript. This will help gain basic HTML5/CSS3/JavaScript programming skills, and is an entry point into both the Web application and Windows Store apps training paths. The course focuses on using HTML5/CSS3/JavaScript to implement programming logic, define and use variables, perform looping and branching, develop user interfaces, capture and validate user input, store data, and create well-structured application. Further, this unit covers the skills, knowledge, and attitude required in developing HTML5 Websites. Students will also learn to develop Windows forms using VB.NET and .NET Framework 4.5 tools and technologies. The focus will be on coding activities that enhance the performance and scalability of the Web site application. ASP.NET MVC will be introduced and compared with Web Forms so that students know when each should/could be used."
 let cook_desc = ""
 let tour_desc = ""
 let anme_desc = ""
+
+desc.innerHTML = prog_desc
